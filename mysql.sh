3145c942de82c1c4af3adcf823b05e779ef37401
anilt01 <tanil010398@gmail.com>
2022-12-06 17:09:16 -0500

    setting up mysql

--- mysql.sh
+++ mysql.sh
@@ -4,8 +4,8 @@ echo Download mysqlrepo file
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
 StatusCheck $?
 
-echo disable previous/installed bersion of mysql in the server
-dnf module disable mysql &>>LOG_FILE
+echo disable previous/installed version of mysql in the server
+dnf module disable mysql -y &>>LOG_FILE
 StatusCheck $?
 
 echo Install mysql server 5.7 version
