87c081b9cd00e1823b5f24228ca18a4823a8122c
anilt01 <tanil010398@gmail.com>
2022-12-06 15:50:28 -0500

    setting up mongodb

--- redis.sh
+++ redis.sh
@@ -2,22 +2,22 @@ LOG_FILE=/tmp/redis
 source=common.sh
 
 echo install redis repo files
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>LOG_FILE
 StatusCheck $?
 
 echo enabke redis module
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>>LOG_FILE
 StatusCheck $?
 
 echo install Redis
-yum install redis -y
+yum install redis -y &>>LOG_FILE
 StatusCheck $?
 
 echo Update Listen address
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>LOG_FILE
 StatusCheck $?
 
 echo start and enable redis service
-systemctl enable redis
-systemctl start redis
+systemctl enable redis &>>LOG_FILE
+systemctl start redis &>>LOG_FILE
 StatusCheck $?
