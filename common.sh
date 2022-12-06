afebc49dc818082d6dd838112240925444c209e8
anilt01 <tanil010398@gmail.com>
2022-12-06 16:17:53 -0500

    setting up user

--- common.sh
+++ common.sh
@@ -43,7 +43,7 @@ NODEJS () {
   StatusCheck $?
 
   echo Setup systemD service file
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.intenal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/g' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/g' /home/roboshop/${COMPONENT}/systemd.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.intenal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/g' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/g' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/g' /home/roboshop/${COMPONENT}/systemd.service
   StatusCheck $?
 
   echo setup ${COMPONENT} service
