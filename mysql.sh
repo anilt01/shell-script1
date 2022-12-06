c757991e395d411effb33115199af3d2e186f200
anilt01 <tanil010398@gmail.com>
2022-12-06 17:28:03 -0500

    setting up mysql

--- mysql.sh
+++ mysql.sh
@@ -17,3 +17,14 @@ systemctl enable mysqld &>>LOG_FILE
 systemctl start mysqld &>>LOG_FILE
 StatusCheck $?
 
+DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+# As we need to use the secure installation manually we are using different command to login mysql
+echo SET PASSWORD FOR 'root'@'localhost' = PASSWORD('ROBOSHOP_MYSQL_PASSWORD');
+FLUSH PRIVILEGES; >/tmp/root-pass.sql
+
+echo change mysql root password
+mysql -uroot -p"$DEFAULT_PASSWORD" </tmp/root-pass.sql
+
+
+
