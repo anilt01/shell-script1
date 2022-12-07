4a76994fbd2c2639a8f3421ffccc33b2b15192dc
anilt01 <tanil010398@gmail.com>
2022-12-06 21:26:37 -0500

    setting up mysql

--- mysql.sh
+++ mysql.sh
@@ -1,27 +1,28 @@
 LOG_FILE=/tmp/mysql
 source common.sh
-echo Download mysqlrepo file
+
+echo "Download mysqlrepo file"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 StatusCheck $?
 
-echo disable previous/installed version of mysql in the server
+echo "disable previous/installed version of mysql in the server"
 dnf module disable mysql -y &>>$LOG_FILE
 StatusCheck $?
 
-echo Install mysql server 5.7 version
+echo "Install mysql server 5.7 version"
 yum install mysql-community-server -y &>>$LOG_FILE
 StatusCheck $?
 
-echo enable and start mysql server
+echo "enable and start mysql server"
 systemctl enable mysqld &>>$LOG_FILE
-systemctl start mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
 StatusCheck $?
 
-DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') &>>$LOG_FILE
+DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 # As we need to use the secure installation manually we are using different command to login mysql
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql &>>$LOG_FILE
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 
 echo change mysql root password
@@ -31,22 +32,23 @@ if [ $? -ne 0 ]; then
  StatusCheck $?
 fi
 
-echo "show plugins" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG_FILE
+echo 'show plugins' | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG_FILE
 if [ $? -eq 0 ]; then
- echo uninstall password validation plugins
+ echo "uninstall password validation plugins"
  echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
  StatusCheck $?
 fi
+
 echo Download Schema files
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
 StatusCheck $?
 
 echo unzip schema files
 cd /tmp
-unzip mysql.zip
+unzip -o mysql.zip &>>$LOG_FILE
 StatusCheck $?
 
 echo load schema files
 cd mysql-main
-mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql
+mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
 StatusCheck $?
