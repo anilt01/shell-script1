9b2fc02607c43b13698449e64ba668539aba315f
anilt01 <tanil010398@gmail.com>
2022-12-06 20:28:21 -0500

    setting up mysql

--- mysql.sh
+++ mysql.sh
@@ -17,14 +17,37 @@ systemctl enable mysqld &>>LOG_FILE
 systemctl start mysqld &>>LOG_FILE
 StatusCheck $?
 
-DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') &>>LOG_FILE
 
 # As we need to use the secure installation manually we are using different command to login mysql
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql &>>LOG_FILE
+
 
 echo change mysql root password
-mysql -uroot --connect-expired-password -p"$DEFAULT_PASSWORD" </tmp/root-pass.sql
+echo "show databases;" |mysql -uroot -p'{$ROBOSHOP_DEFAULT_PASSWORD}' &>>LOG_FILE
+if [ $? -ne 0 ]; then
+ mysql -uroot --connect-expired-password -p"$DEFAULT_PASSWORD" </tmp/root-pass.sql &>>LOG_FILE
+ StatusCheck $?
+fi
 
+echo "show plugins" | mysql -uroot -p'{$ROBOSHOP_DEFAULT_PASSWORD}' | grep validate_password &>>LOG_FILE
+if [ $? -eq 0 ] then;
+ echo uninstall passowrd validation plugins
+ echo "uninstall plugin validate_password;" | mysql -uroot -p'{$ROBOSHOP_DEFAULT_PASSWORD}' &>>LOG_FILE
+ StatusCheck $?
+fi
 
+echo Download Schema files
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>LOG_FILE
+StatusCheck $?
 
+echo unzip schema files
+cd /tmp
+unzip mysql.zip
+StatusCheck $?
+
+echo load schema files
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql
+StatusCheck $?
