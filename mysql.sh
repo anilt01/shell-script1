c2e4243630937096101706ac3bd6c8d444cb2932
anilt01 <tanil010398@gmail.com>
2022-12-06 21:18:24 -0500

    setting up mysql

--- mysql.sh
+++ mysql.sh
@@ -1,44 +1,44 @@
 LOG_FILE=/tmp/mysql
 source common.sh
 echo Download mysqlrepo file
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 StatusCheck $?
 
 echo disable previous/installed version of mysql in the server
-dnf module disable mysql -y &>>LOG_FILE
+dnf module disable mysql -y &>>$LOG_FILE
 StatusCheck $?
 
 echo Install mysql server 5.7 version
-yum install mysql-community-server -y &>>LOG_FILE
+yum install mysql-community-server -y &>>$LOG_FILE
 StatusCheck $?
 
 echo enable and start mysql server
-systemctl enable mysqld &>>LOG_FILE
-systemctl start mysqld &>>LOG_FILE
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld &>>$LOG_FILE
 StatusCheck $?
 
-DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') &>>LOG_FILE
+DEFAULT_PASSWORD=$( grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') &>>$LOG_FILE
 
 # As we need to use the secure installation manually we are using different command to login mysql
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql &>>LOG_FILE
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql &>>$LOG_FILE
 
 
 echo change mysql root password
-echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQl_PASSWORD} &>>LOG_FILE
+echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQl_PASSWORD} &>>$LOG_FILE
 if [ $? -ne 0 ]; then
- mysql -uroot --connect-expired-password -p${DEFAULT_PASSWORD} </tmp/root-pass.sql &>>LOG_FILE
+ mysql -uroot --connect-expired-password -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
  StatusCheck $?
- fi
+fi
 
-echo "show plugins" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>LOG_FILE
+echo "show plugins" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG_FILE
 if [ $? -eq 0 ]; then
- echo uninstall passowrd validation plugins
- echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>LOG_FILE
+ echo uninstall password validation plugins
+ echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
  StatusCheck $?
 fi
 echo Download Schema files
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>LOG_FILE
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
 StatusCheck $?
 
 echo unzip schema files
@@ -48,5 +48,5 @@ StatusCheck $?
 
 echo load schema files
 cd mysql-main
-mysql -u root -pRoboShop@1 <shipping.sql
+mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql
 StatusCheck $?
