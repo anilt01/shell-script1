8b096b46fcb67281b5765e67f8362a5d2d99e34a
anilt01 <tanil010398@gmail.com>
2022-12-07 16:29:01 -0500

    setting up shipping

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,22 +1,12 @@
 source common.sh
 LOG_FILE=/tmp/rabbitmq
 
-#echo Install erlang
-#yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
-# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-
-
-
-echo "Download erlang dependencies for rabbitmq"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOG_FILE
+echo Install erlang
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
 StatusCheck $?
 
-#echo "Install erlang"
-#yum install erlang
-#StatusCheck $?
-
-echo "Download rabbitmq repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+echo Install rabbitmq
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
 StatusCheck $?
 
 echo "Install Rabbitmq"
