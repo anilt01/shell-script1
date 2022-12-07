023c09d950b9dfeec2041f5c0a019209f99bc646
anilt01 <tanil010398@gmail.com>
2022-12-07 17:02:35 -0500

    setting up rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,7 +6,13 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 StatusCheck $?
 
 echo Install erlang and Rabbitmq
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+StatusCheck $?
+
+yum install erlang -y
+StatusCheck $?
+
+yum install rabbitmq-server -y
 StatusCheck $?
 
 echo "enable and start rabbitmq"
