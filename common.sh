ddacc089555bc1892c297d3b13bb0a9e2ee320d7
anilt01 <tanil010398@gmail.com>
2022-12-08 17:07:38 -0500

    setting up dispatch

--- common.sh
+++ common.sh
@@ -39,7 +39,7 @@ APP_PREREQ() {
 
 SYSTEMD_SETUP() {
   echo Setup systemD service file
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.intenal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/g' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/g' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/g' -e 's/CARTENDPOINT/cart.roboshop.internal/g' -e 's/DBHOST/mysql.roboshop.internal/g' -e 's/CARTHOST/cart.roboshop.internal/g' -e 's/USERHOST/user.roboshop.internal/g' -e 's/AMQPHOST /rabbitmq.roboshop.internal/g' /home/roboshop/${COMPONENT}/systemd.service
+  sed -i  -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/g' -e 's/MONGO_DNSNAME/mongodb.roboshop.intenal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/g' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/g' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/g' -e 's/CARTENDPOINT/cart.roboshop.internal/g' -e 's/DBHOST/mysql.roboshop.internal/g' -e 's/CARTHOST/cart.roboshop.internal/g' -e 's/USERHOST/user.roboshop.internal/g' -e 's/AMQPHOST /rabbitmq.roboshop.internal/g' /home/roboshop/${COMPONENT}/systemd.service
   StatusCheck $?
 
   echo setup ${COMPONENT} service
@@ -135,5 +135,5 @@ GOLANG() {
   go build &>>$LOG_FILE
   StatusCheck $?
 
-
+  SYSTEMD_SETUP
 }
