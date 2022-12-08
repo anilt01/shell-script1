accaca75fdf645f432676e95872cbbbd76ed64bb
anilt01 <tanil010398@gmail.com>
2022-12-08 18:09:21 -0500

    setting up dispatch

--- frontend.sh
+++ frontend.sh
@@ -25,6 +25,9 @@ mv frontend-main/static/* . &>>$LOG_FILE
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatusCheck $?
 
+echo "update roboshop.conf file"
+sed -i -e '/catalogue/  s/localhost/catalogue.roboshop.internal/g' -e '/user/  s/localhost/user.roboshop.internal/g' -e '/cart/  s/localhost/cart.roboshop.internal/g' -e '/shipping/  s/localhost/shipping.roboshop.internal/g' -e '/payment/  s/localhost/payment.roboshop.internal/g' /etc/nginx/default.d/roboshop.conf
+
 echo restarting nginx
 systemctl restart nginx &>>$LOG_FILE
 StatusCheck $?
